387a6fce223fd0fca8984298ffe48cad772d89a0
lokesh <kottelokesh789@gmail.com>
2021-12-15 16:47:16 +0530

    ADDED: GamblerGameUc2

--- src/com/bridgelabz/gamblergame/GamblerGame.java
+++ src/com/bridgelabz/gamblergame/GamblerGame.java
@@ -3,5 +3,31 @@ package com.bridgelabz.gamblergame;
 public class GamblerGame {
 	static int STAKE_AMOUNT=100;
 	static int BET_AMOUNT=1;
+	static int WON=1;
+	static int LOST=0;
+	public static void main(String[] args) {
+		int play = (int) Math.floor(Math.random() * 10) % 2;
+		 System.out.println("Gamblers play:"+play);
+		 int cashInHand = STAKE_AMOUNT;
+		 switch(play) {
+		 	case 1:
+		 		 cashInHand = cashInHand + BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+cashInHand);
+		 		 break;
+		 	case 2:
+		 		 cashInHand = cashInHand - BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+cashInHand);
+		 		 break;
+		 	default:
+		 		 System.out.println("default");
+		 }
+		 if(play == 1) {
+			 System.out.println("Player won the first play");
+		 }
+		 else {
+			 System.out.println("Player lost the first play");
+		 }
 
+
+	}
 }
